a838f85ee0fe1945818a76571b37d7281a6d6b90
Kristen M <kristenmayorga@gmail.com>
2020-08-12 22:07:14 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static("public"));
+
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   // Note that we save the animalsArray as filteredResults here:
@@ -101,6 +104,7 @@ app.post("/api/animals", (req, res) => {
   } else {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
+    
   }
 });
 
@@ -119,6 +123,17 @@ function validateAnimal(animal) {
   }
   return true;
 }
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
